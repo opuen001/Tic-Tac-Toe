954fb0a80265fe1fb0b6b6101bf1fbd16ded9682
ChickensRock4 <opuen001@ucr.edu>
2024-12-18 23:46:04 -0800

    The Tic Tac Toe game is now functioning

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+int = __builtins__.int
+
 def printBoard(board):
     print(board[0], " | ", board[1], " | ", board[2])
     print("-------------")
@@ -23,11 +25,18 @@ def detWin(letter, board):
     else:
         return False
 
-def getInput():
+def getInput(board):
     n = -1
+
     while (0 > n or n > 9):
         n = int(input("Enter a number 1 to 9\n"))
         print()
+
+        if(0<= n <= 9):
+            if(board[n - 1] == 'X' or board[n-1] == 'O'):
+                print("That square is taken, please choose another")
+                n = -1 #so that the while loop runs again
+            
     return n
 
 def clearScreen():
@@ -35,57 +44,31 @@ def clearScreen():
     print()
     print()
 
+def nextTurn(turn):
+    if(turn == 'O'):
+        return 'X'
+    else:
+        return 'O'
+
+def gameOver(board):
+    if(detWin('O', board) or detWin('X', board)):
+        return True
+    else:
+        return ((board[0] != '1') and (board[1] != '2') and (board[2] != '3')
+                and (board[3] != '4') and (board[4] != '5') and (board[5] != '6')
+                and (board[6] != '7') and (board[7] != '8') and (board[8] != '9'))
+
 board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+gameEnd = False
+turn = 'X'
+
+while not (gameEnd):
+    turn = nextTurn(turn)
+    printBoard(board)
+    pInput = getInput(board)
+    board[pInput - 1] = turn
+
+    gameEnd = gameOver(board)
+
 printBoard(board)
-input = getInput()
-
-if(input == -1):
-    #1
-    clearScreen()
-    board = ['X', 'X', 'X', '4', '5', '6', '7', '8', '9']
-    printBoard(board)
-    print(detWin('X', board))
-    #2
-    clearScreen()
-    board = ['1', '2', '3', 'X', 'X', 'X', '7', '8', '9']
-    printBoard(board)
-    print(detWin('X', board))
-    #3
-    clearScreen()
-    board = ['1', '2', '3', '4', '5', '6', 'X', 'X', 'X']
-    printBoard(board)
-    print(detWin('X', board))
-
-    #4
-    clearScreen()
-    board = ['X', '2', '3', 'X', '5', '6', 'X', '8', '9']
-    printBoard(board)
-    print(detWin('X', board))
-    #5
-    clearScreen()
-    board = ['1', 'X', '3', '4', 'X', '6', '7', 'X', '9']
-    printBoard(board)
-    print(detWin('X', board))
-    #6
-    clearScreen()
-    board = ['1', '2', 'X', '4', '5', 'X', '7', '8', 'X']
-    printBoard(board)
-    print(detWin('X', board))
-
-    #7
-    clearScreen()
-    board = ['X', '2', '3', '4', 'X', '6', '7', '8', 'X']
-    printBoard(board)
-    print(detWin('X', board))
-    #8
-    clearScreen()
-    board = ['1', '2', 'X', '4', 'X', '6', 'X', '8', '9']
-    printBoard(board)
-    print(detWin('X', board))
-
-    #9
-    clearScreen()
-    board = ['1', '2', 'X', '4', 'X', '6', '7', '8', '9']
-    printBoard(board)
-    print(detWin('X', board))
-
+print(turn, "'s won!", sep='')
